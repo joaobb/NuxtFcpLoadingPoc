a8b8140525ee1d98f68ee640b7d0d4983257a0dd
joaobb <joao.pedro.barbosa@ccc.ufcg.edu.br>
2023-03-30 10:01:26 -0300

    Add conditional root page loading; Add loading page layout

---
+++ layouts/loaded.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <div id="custom-loading-bar" style="z-index: 1"></div>
+    <Nuxt />
+  </div>
+</template>
---
+++ pages/layout_loading.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <PageContent />
+  </div>
+</template>
+
+<script>
+import PageContent from '../components/PageContent'
+
+export default {
+  name: 'LoadingLayoutPage',
+  components: {
+    PageContent,
+  },
+  layout: 'loaded',
+}
+</script>
